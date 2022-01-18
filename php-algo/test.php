9b27e09e2186a424d543c59de3289506520e6ccb
Timothe00 <88444565+Timothe00@users.noreply.github.com>
2022-01-18 22:55:12 +0000

    Update test.php

--- php-algo/test.php
+++ php-algo/test.php
@@ -22,7 +22,7 @@ if(!empty($_GET)){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Welcome to my Page</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
